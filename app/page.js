286e32f84258e639d4b0a2f33ca0bea798f343ba
jiantengtoo <jiantengtoo@gmail.com>
2023-10-25 16:52:56 +0800

    test

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
-          test dev branch protection
+          test dev branch protection 2
         </p>
         <div>
           <a
